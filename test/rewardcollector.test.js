2e145d903cf615bfc5464fae05a82847473e041c
siem <siemarell@gmail.com>
2018-03-15 18:01:18 +0300

    +2 tests. 1 failed

--- test/rewardcollector.test.js
+++ test/rewardcollector.test.js
@@ -1,6 +1,6 @@
 const RewardCollector = artifacts.require('RewardCollector')
 
-contract('Reward Collector', ([lawyer, benef]) => {
+contract('Reward Collector', ([lawyer, benef, faucet]) => {
   describe('creation', () => {
     it('should create contract', async () => {
       const rewardInstance = await RewardCollector.new(
@@ -57,7 +57,7 @@ contract('Reward Collector', ([lawyer, benef]) => {
       //Send 10 ether to contract address
       await rewardInstance.sendTransaction({
         value: web3.toWei(10, 'ether'),
-        from: lawyer
+        from: faucet
       })
       const balanceBefore = await web3.eth.getBalance(lawyer)
       
@@ -74,6 +74,56 @@ contract('Reward Collector', ([lawyer, benef]) => {
       //Check if layer got correct volume of ether
       const balanceAfter = await web3.eth.getBalance(lawyer)
       assert.equal(balanceAfter.sub(balanceBefore).toNumber(), web3.toWei(3, 'ether') - comission)
+    })
+
+    it('should allow to withdraw beneficiares part', async () => {
+
+      //Send 10 ether to contract address
+      await rewardInstance.sendTransaction({
+        value: web3.toWei(10, 'ether'),
+        from: faucet
+      })
+      const balanceBefore = await web3.eth.getBalance(benef)
+      console.log(balanceBefore.toNumber())
+      //Withdraw layer part
+      const receipt = await rewardInstance.withdrawBeneficiaries()
+      assert(receipt)
+      console.log(receipt)
+      //Calculate comission
+      const gasUsed = receipt.receipt.gasUsed
+      const tx = await web3.eth.getTransaction(receipt.tx)
+      const gasPrice = tx.gasPrice
+      const comission = gasUsed * gasPrice
+     
+      //Check if benef got correct volume of ether
+      const balanceAfter = await web3.eth.getBalance(benef)
+      console.log(balanceAfter.toNumber())
+      assert.equal(balanceAfter.sub(balanceBefore).toNumber(), web3.toWei(7, 'ether') - comission)
+    })
+
+    it('should allow to withdraw twice', async () => {
+
+      //Send 10 ether to contract address
+      await rewardInstance.sendTransaction({
+        value: web3.toWei(10, 'ether'),
+        from: faucet
+      })
+      const balanceBefore = await web3.eth.getBalance(benef)
+      
+      //Withdraw first time
+      let receiptL = await rewardInstance.withdrawLawyer()
+      let receiptB = await rewardInstance.withdrawBeneficiaries()
+      assert(receiptL)
+      assert(receiptB)
+
+      //Withdraw second time
+      try{
+        receiptL = await rewardInstance.withdrawLawyer()
+        receiptB = await rewardInstance.withdrawBeneficiaries()  
+      }catch(e){
+        return
+      }
+      assert.fail()
     })
   })
 })
