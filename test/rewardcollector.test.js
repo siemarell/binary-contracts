277d46c89e2a6075f99643c640f7e05570cdbbc0
siem <siemarell@gmail.com>
2018-03-15 13:17:23 +0300

    hometask

--- test/rewardcollector.test.js
+++ test/rewardcollector.test.js
@@ -1,6 +1,6 @@
-const RewardCollector = artifacts.require('./RewardCollector.sol')
+const RewardCollector = artifacts.require('RewardCollector')
 
-contract('Reward Collector', ([lawyer, benef, layer]) => {
+contract('Reward Collector', ([lawyer, benef]) => {
   describe('creation', () => {
     it('should create contract', async () => {
       const rewardInstance = await RewardCollector.new(
@@ -12,9 +12,24 @@ contract('Reward Collector', ([lawyer, benef, layer]) => {
     })
 
     it('should reject more than 100%', async () => {
-      assert.fail() // home work
+      try {
+        const rewardInstance = await RewardCollector.new(10001, lawyer, benef)
+      }catch(e){
+        return
+      }
+      assert.fail();      
+      //assert(!rewardInstance.address) // home work
     })
 
+    it('should reject negative %', async () => {
+      try {
+        const rewardInstance = await RewardCollector.new(-1, lawyer, benef)
+      }catch(e){
+        return
+      }
+      assert.fail();      
+      //assert(!rewardInstance.address) // home work
+    })
     // TODO: Другие тесты проверки результатов создания контракта
   })
 
@@ -28,7 +43,7 @@ contract('Reward Collector', ([lawyer, benef, layer]) => {
       const balanceBefore = await web3.eth.getBalance(rewardInstance.address)
       assert(await rewardInstance.sendTransaction({
         value: web3.toWei(1, 'ether'),
-        from: layer
+        from: lawyer
       }))
 
       const balanceAfter = await web3.eth.getBalance(rewardInstance.address)
@@ -39,15 +54,26 @@ contract('Reward Collector', ([lawyer, benef, layer]) => {
 
     it('should allow to withdraw laywer part', async () => {
 
+      //Send 10 ether to contract address
       await rewardInstance.sendTransaction({
         value: web3.toWei(10, 'ether'),
-        from: layer
+        from: lawyer
       })
-      
       const balanceBefore = await web3.eth.getBalance(lawyer)
-      assert(await rewardInstance.withdrawLawyer())
+      
+      //Withdraw layer part
+      const receipt = await rewardInstance.withdrawLawyer()
+      assert(receipt)
+
+      //Calculate comission
+      const gasUsed = receipt.receipt.gasUsed
+      const tx = await web3.eth.getTransaction(receipt.tx)
+      const gasPrice = tx.gasPrice
+      const comission = gasUsed * gasPrice
+     
+      //Check if layer got correct volume of ether
       const balanceAfter = await web3.eth.getBalance(lawyer)
-      assert.equal(balanceAfter.sub(balanceBefore).toNumber(), web3.toWei(3, 'ether'))
+      assert.equal(balanceAfter.sub(balanceBefore).toNumber(), web3.toWei(3, 'ether') - comission)
     })
   })
 })
